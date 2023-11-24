fa8bccbaab3dd575b646fb6f99c75cea8ee3403b
steeve0304 <zychsteeve4@gmail.com>
2023-11-24 14:37:59 +0100

    add img darkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,48 @@ burger.addEventListener("click", function(){
     }
 });
 
-let darkModeToggle = document.getElementById("dark-mode-toggle");
+/*let darkModeToggle = document.getElementById("dark-mode-toggle");
 darkModeToggle.addEventListener("click", function() {
     let body = document.body;
     let header = document.querySelector('header');
     body.classList.toggle('dark-mode');
     header.classList.toggle('dark-mode');
+});*/
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Check if dark mode is enabled in localStorage
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    const images = document.querySelectorAll('.lozad');
+
+    // Apply dark mode if it was enabled
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+    }
+
+    // Initialize lozad
+    const observer = lozad(images, {
+        load: function(el) {
+            el.src = document.body.classList.contains('dark-mode') ? el.getAttribute('data-src-dark') : el.getAttribute('data-src');
+        },
+        loaded: function(el) {
+            el.classList.add('faded-in');
+        }
+    });
+
+    // Start observing images
+    observer.observe();
+
+    // Code to switch modes
+    const switchButton = document.getElementById('dark-mode-toggle');
+    switchButton.addEventListener('click', function() {
+        const isDarkMode = document.body.classList.toggle('dark-mode');
+        // Update image src for current mode and reload lozad
+        images.forEach(img => {
+            img.src = isDarkMode ? img.getAttribute('data-src-dark') : img.getAttribute('data-src');
+            observer.triggerLoad(img);
+        });
+        // Store mode in localStorage
+        localStorage.setItem('darkMode', isDarkMode.toString());
+    });
 });
+
