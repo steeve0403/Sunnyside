13b684dac4f281a5af2f8867600fcc534a956964
steeve0304 <zychsteeve4@gmail.com>
2023-11-28 16:42:57 +0100

    add new animation to load

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,22 +18,18 @@ darkModeToggle.addEventListener("click", function() {
     header.classList.toggle('dark-mode');
 });
 */
-
 window.addEventListener('DOMContentLoaded', (event) => {
-    // Check if dark mode is enabled in localStorage
     const isDarkMode = localStorage.getItem('darkMode') === 'true';
     const images = document.querySelectorAll('.lozad');
-    const overlay = document.querySelector('.overlay');
+    const overlay = document.getElementById('overlay');
     const loader = document.createElement('div');
     loader.classList.add('loader');
     document.body.appendChild(loader);
 
-    // Apply dark mode if it was enabled
     if (isDarkMode) {
         document.body.classList.add('dark-mode');
     }
 
-    // Initialize lozad
     const observer = lozad(images, {
         load: function(el) {
             el.src = document.body.classList.contains('dark-mode') ? el.getAttribute('data-src-dark') : el.getAttribute('data-src');
@@ -43,57 +39,40 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
-    // Start observing images
     observer.observe();
 
-    // Code to switch modes
     const switchButton = document.getElementById('dark-mode-toggle');
     switchButton.addEventListener('click', function() {
-        // Show the loader before changing the design
         showLoader();
 
-        // Start the fade out animation for the loader and overlay
         setTimeout(() => {
             hideLoader();
             const isDarkMode = document.body.classList.toggle('dark-mode');
-
-            // Update image src for the current mode and reload lozad
-            images.forEach(img => {
-                img.src = isDarkMode ? img.getAttribute('data-src-dark') : img.getAttribute('data-src');
-                observer.triggerLoad(img);
-            });
-
-            // Store mode in localStorage
+            updateImages(isDarkMode);
             localStorage.setItem('darkMode', isDarkMode.toString());
-
-            // Start the overlay fade out animation
-            overlay.style.opacity = '0';
-
-            // Hide the overlay after the animation is complete
-            setTimeout(() => {
-                overlay.style.pointerEvents = 'none';
-            }, 1000);
         }, 1000);
     });
 
-    // Function to show the loader
     function showLoader() {
+        overlay.style.opacity = '1';
+        overlay.style.pointerEvents = 'auto';
         loader.style.opacity = '1';
         loader.style.visibility = 'visible';
-        loader.addEventListener('animationstart', onAnimationStart);
     }
 
-    // Function to hide the loader
     function hideLoader() {
+        overlay.style.opacity = '0';
+        overlay.style.pointerEvents = 'none';
         loader.style.opacity = '0';
         loader.style.visibility = 'hidden';
-        loader.removeEventListener('animationstart', onAnimationStart);
     }
 
-    // Callback function for the 'animationstart' event
-    function onAnimationStart(event) {
-        // Vous pouvez ajouter des actions supplémentaires au début de l'animation si nécessaire
-        console.log('Animation started:', event.animationName);
+    function updateImages(isDarkMode) {
+        images.forEach(img => {
+            img.src = isDarkMode ? img.getAttribute('data-src-dark') : img.getAttribute('data-src');
+            observer.triggerLoad(img);
+        });
     }
 });
+
 
